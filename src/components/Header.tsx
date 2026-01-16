f18964697f286fbe69e7d4c272ae6d1772697d25
Mateus Dal Bianco <mateus@dlbn.co>
2026-01-16 13:39:03 +0100

    feat: tweak logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
     <header className="navbar bg-base-100 border-b border-base-200 px-4">
       <div className="flex-1">
-        <Link href="/" className="text-xl font-bold font-mono">
-          issues.cash
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-xl font-bold font-mono"
+        >
+          <Image
+            width={32}
+            height={32}
+            src="/logo.svg"
+            alt="issues.cash logo"
+          />
         </Link>
       </div>
     </header>
