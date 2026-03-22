65a7c1e84deae13b1860f9bac8fa38dab71e091a
Mateus Dal Bianco <mateus@dlbn.co>
2026-03-22 19:21:33 +0100

    feat: add get started; tweak footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,29 +5,25 @@ export function Footer() {
   const network = getWebsiteNetworkName();
 
   return (
-    <footer className="footer footer-center p-4 bg-base-100 text-base-content border-t border-base-200">
-      <aside>
-        <p className="font-mono text-sm opacity-60">
-          issues.cash
-          <span className="mx-2">·</span>
-          <span>{network}</span>
-          <span className="mx-2">·</span>
-          <Link href="/terms" className="hover:underline">
-            Terms
-          </Link>
-          <span className="mx-2">·</span>
-          <Link href="/privacy" className="hover:underline">
-            Privacy
-          </Link>
-          <span className="mx-2">·</span>
-          <Link
-            href="https://github.com/dlbn.co/issues.cash"
-            className="hover:underline"
-          >
-            GitHub
-          </Link>
-        </p>
-      </aside>
+    <footer className="footer footer-center w-full p-4 bg-base-100 text-base-content border-t border-base-200">
+      <div className="flex flex-col md:flex-row flex-wrap gap-2 font-mono text-sm opacity-60">
+        <span>issues.cash · {network}</span>
+        <span className="hidden md:block">·</span>
+        <Link href="/terms" className="hover:underline">
+          Terms
+        </Link>
+        <span className="hidden md:block">·</span>
+        <Link href="/privacy" className="hover:underline">
+          Privacy
+        </Link>
+        <span className="hidden md:block">·</span>
+        <Link
+          href="https://github.com/dlbn.co/issues.cash"
+          className="hover:underline"
+        >
+          GitHub
+        </Link>
+      </div>
     </footer>
   );
 }
